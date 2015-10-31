5bf9091fe09cf42198d3c7c192fa98ff04e6f17e
unknown <abhina@ncsu.edu>
2015-10-31 10:05:22 -0400

    Solution for program 2

--- src/Solution.java
+++ src/Solution.java
@@ -2,4 +2,10 @@
  * Created by Abhinav on 10/31/2015.
  */
 public class Solution {
+    public static void main (String[] args){
+        StringSolutions sol = new StringSolutions();
+        boolean result= sol.permutation("great","treag");
+        System.out.println(result);
+    }
+
 }
--- src/StringSolutions.java
+++ src/StringSolutions.java
@@ -1,9 +1,12 @@
+import java.util.Arrays;
+
 /**
  * Created by Abhinav on 10/31/2015.
  */
 //This class contains functions for all the string related questions
 
 public class StringSolutions {
+    //Checking if a string contains all unique characters
     public boolean unique(String input) {
         if (input.length() > 26) {
             return false;
@@ -20,4 +23,27 @@ public class StringSolutions {
         }
     }
 
+
+    // Function to check if two strings are permutaions of each other
+    /*
+    sort both of the strings and macth
+    */
+
+    public boolean permutation(String string1, String string2) {
+        char[] charArray1 = new char[string1.length()];
+        char[] charArray2 = new char[string2.length()];
+        charArray1 = string1.toCharArray();
+        charArray2 = string2.toCharArray();
+        Arrays.sort(charArray1);
+        Arrays.sort(charArray2);
+        string1 = new String(charArray1);
+        string2 = new String(charArray2);
+        //System.out.println(string1);
+        //System.out.println(string2);
+        if (string1.equals(string2)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
